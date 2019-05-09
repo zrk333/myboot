3c16bc10a9a7548e3667c31177e3347e69f9d5b9
zhrk <zhrk@jhwx.com>
2019-05-09 16:26:23 +0800

    lambada

--- src/main/java/com/zrk/controller/UserController.java
+++ src/main/java/com/zrk/controller/UserController.java
@@ -2,16 +2,21 @@ package com.zrk.controller;
 
 import com.zrk.pojo.User;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description:
  * @Author: zrk
  * @Date: 2019/5/4
  */
+@Slf4j
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -24,6 +29,13 @@ public class UserController {
         user.setId(id);
         user.setName("张三");
         user.setPassword("123456");
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user);
+        userList.add(new User());
+        userList.forEach(u -> System.out.println(u));
+
         return user;
     }
 
